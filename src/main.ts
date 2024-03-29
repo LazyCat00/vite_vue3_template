5ebf240de3e1113f8b18701714e590bd32f48cde
LazyCat00 <9533511@qq.com>
2024-03-29 21:07:30 +0800

    feat: element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-createApp(App).mount('#app')
+// 引入element - plus插件与样式
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
+// 配置element - plus国际化
+// @ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// 获取应用实例对象
+const app = createApp(App)
 
-const fun = () => {
-  console.log('我是嫩爹!')
-}
-
-fun()
+// 安装element-plus插件
+app.use(ElementPlus, {
+  locale: zhCn//element-plus国际化配置
+})
+// 将应用挂载到挂载点上
+app.mount('#app')
