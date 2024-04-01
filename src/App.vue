318ed8bfb7074b278dada97e3095d71ad18eee13
LazyCat00 <9533511@qq.com>
2024-04-01 10:28:51 +0800

    feat: src alias

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,6 @@
 <template>
   <div>
-    <el-button type="primary" :icon="Plus">Primary</el-button>
-
-
-    <div class="flex items-center mb-4">
-      <el-radio-group v-model="small" class="mr-4">
-        <el-radio-button :value="false">default</el-radio-button>
-        <el-radio-button :value="true">small</el-radio-button>
-      </el-radio-group>
-      <div>
-        background:
-        <el-switch v-model="background" class="ml-2" />
-      </div>
-      <div class="ml-4">
-        disabled: <el-switch v-model="disabled" class="ml-2" />
-      </div>
-    </div>
-
-    <hr class="my-4" />
-
-    <div class="demo-pagination-block">
-      <div class="demonstration">Total item count</div>
-      <el-pagination v-model:current-page="currentPage1" :page-size="100" :small="small" :disabled="disabled"
-        :background="background" layout="total, prev, pager, next" :total="1000" @size-change="handleSizeChange"
-        @current-change="handleCurrentChange" />
-    </div>
-    <div class="demo-pagination-block">
-      <div class="demonstration">Change page size</div>
-      <el-pagination v-model:current-page="currentPage2" v-model:page-size="pageSize2"
-        :page-sizes="[100, 200, 300, 400]" :small="small" :disabled="disabled" :background="background"
-        layout="sizes, prev, pager, next" :total="1000" @size-change="handleSizeChange"
-        @current-change="handleCurrentChange" />
-    </div>
-    <div class="demo-pagination-block">
-      <div class="demonstration">Jump to</div>
-      <el-pagination v-model:current-page="currentPage3" v-model:page-size="pageSize3" :small="small"
-        :disabled="disabled" :background="background" layout="prev, pager, next, jumper" :total="1000"
-        @size-change="handleSizeChange" @current-change="handleCurrentChange" />
-    </div>
-    <div class="demo-pagination-block">
-      <div class="demonstration">All combined</div>
-      <el-pagination v-model:current-page="currentPage4" v-model:page-size="pageSize4"
-        :page-sizes="[100, 200, 300, 400]" :small="small" :disabled="disabled" :background="background"
-        layout="total, sizes, prev, pager, next, jumper" :total="400" @size-change="handleSizeChange"
-        @current-change="handleCurrentChange" />
-    </div>
-
+    <h1>Hello</h1>
   </div>
 </template>
 <script setup lang="ts">
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 // 引入element - plus插件与样式
-import ElementPlus from 'element-plus';
+import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // 配置element - plus国际化
 // @ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
@@ -11,7 +11,7 @@ const app = createApp(App)
 
 // 安装element-plus插件
 app.use(ElementPlus, {
-  locale: zhCn//element-plus国际化配置
+  locale: zhCn, //element-plus国际化配置
 })
 // 将应用挂载到挂载点上
 app.mount('#app')
