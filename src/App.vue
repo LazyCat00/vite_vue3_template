7fdd2d9f47492c639094fccb3ee7c5c6d5e50a2e
LazyCat00 <9533511@qq.com>
2024-04-01 15:23:33 +0800

    feat: axios

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div>
     <h1>SVG测试</h1>
-    <svg-icon
-      name="jurassic"
-      color="pink"
-      width="100px"
-      height="100px"
-    ></svg-icon>
+    <svg-icon name="jurassic" color="pink" width="100px" height="100px"></svg-icon>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+</script>
 <style scoped lang="scss">
-div{
-  h1{
-    color:$color;
+div {
+  h1 {
+    color: $color;
   }
 }
 </style>
