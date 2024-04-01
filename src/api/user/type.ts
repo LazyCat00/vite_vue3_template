5450accf300976b0b9057977ece3c3ad0fbf9e33
LazyCat00 <9533511@qq.com>
2024-04-01 15:46:35 +0800

    feat: api

---
+++ src/api/user/type.ts
@@ -0,0 +1,18 @@
+// 登录接口需要携带参数ts类型
+export interface loginFormData {
+    username: string,
+    password: string
+}
+
+// 登录接口返回数据类型
+export interface loginResponseData{
+
+}
+
+
+
+export interface userInfoReponseData {
+
+}
+
+
